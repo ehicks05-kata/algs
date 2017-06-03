a1a42f0f99096cf6a5d24ac9985df6e728d704df
Eric Hicks <ehicks05@yahoo.com>
2017-06-03 10:25:52 -0400

    Finished 1.2.3

--- src/main/java/algs/Algs_1_2_3_Interval2DClient.java
+++ src/main/java/algs/Algs_1_2_3_Interval2DClient.java
@@ -1,9 +1,6 @@
 package algs;
 
-import edu.princeton.cs.algs4.Interval1D;
-import edu.princeton.cs.algs4.Interval2D;
-import edu.princeton.cs.algs4.StdDraw;
-import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.*;
 
 /*
     1.2.3 Write an Interval2D client that takes command-line arguments N , min , and max
@@ -42,6 +39,7 @@ public class Algs_1_2_3_Interval2DClient
             Interval1D ver = new Interval1D(loy, hiy);
             Interval2D rect = new Interval2D(hor, ver);
             rect.draw();
+            StdDraw.text(hor.min() - .01, ver.max() + .01, ""+i);
             intervals[i] = rect;
         }
 
@@ -51,14 +49,34 @@ public class Algs_1_2_3_Interval2DClient
                 Interval2D i1 = intervals[i];
                 Interval2D i2 = intervals[j];
                 if (i1.intersects(i2))
-                    System.out.println(i1 + " " + i2);
-                String[] horVerPair = i1.toString()
+                    System.out.println(i1 + " " + i + " intersects " + j + " " + i2);
+
+                String[] i1horVerPair = i1.toString()
                         .replaceAll("\\[","")
                         .replaceAll("]","")
                         .split(" x ");
-                String[] i1HorCoordPair = horVerPair[0].split(", ");
-                String[] i1VerCoordPair = horVerPair[0].split(", ");
+                String[] i1HorCoordPair = i1horVerPair[0].split(", ");
+                String[] i1VerCoordPair = i1horVerPair[1].split(", ");
+                double i1xlo = Double.parseDouble(i1HorCoordPair[0]);
+                double i1xhi = Double.parseDouble(i1HorCoordPair[1]);
+                double i1ylo = Double.parseDouble(i1VerCoordPair[0]);
+                double i1yhi = Double.parseDouble(i1VerCoordPair[1]);
 
+                String[] i2horVerPair = i2.toString()
+                        .replaceAll("\\[","")
+                        .replaceAll("]","")
+                        .split(" x ");
+                String[] i2HorCoordPair = i2horVerPair[0].split(", ");
+                String[] i2VerCoordPair = i2horVerPair[1].split(", ");
+                double i2xlo = Double.parseDouble(i2HorCoordPair[0]);
+                double i2xhi = Double.parseDouble(i2HorCoordPair[1]);
+                double i2ylo = Double.parseDouble(i2VerCoordPair[0]);
+                double i2yhi = Double.parseDouble(i2VerCoordPair[1]);
+
+                boolean horContained = i1xlo < i2xlo && i1xhi > i2xhi;
+                boolean verContained = i1ylo < i2ylo && i1yhi > i2yhi;
+                if (horContained && verContained)
+                    System.out.println(i1 + " " + i + " contains " + j + " " + i2);
             }
     }
 }
