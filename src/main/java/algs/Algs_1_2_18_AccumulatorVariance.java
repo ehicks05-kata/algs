faf763fb53e06267b71d59012aec98b5fca4a2f8
Eric Hicks <ehicks05@yahoo.com>
2017-06-04 15:04:15 -0400

    Added 1.2.18-1.2.19

--- src/main/java/algs/Algs_1_2_18_AccumulatorVariance.java
+++ src/main/java/algs/Algs_1_2_18_AccumulatorVariance.java
@@ -28,11 +28,41 @@ package algs;
     plementation based on saving the sum of the squares of the numbers.
 * */
 
+import edu.princeton.cs.algs4.StdRandom;
+
 public class Algs_1_2_18_AccumulatorVariance
 {
     public static void main(String[] args) throws Exception
     {
+        int n = 100;
+        double[] array = new double[n];
+        Accumulator accumulator = new Accumulator();
 
+        double total = 0;
+        for (int i = 0; i < n; i++)
+        {
+            double d = StdRandom.uniform();
+            total += d;
+            array[i] = d;
+            accumulator.addDataValue(d);
+        }
+        
+        double mean = total / n;
+
+        double var = 0;
+        for (double a : array)
+            var += (a - mean) * (a - mean);
+        var = var / (n - 1);
+
+        double stddev = Math.sqrt(var);
+
+        System.out.println("my mean:   " + mean);
+        System.out.println("my var:    " + var);
+        System.out.println("my stddev: " + stddev);
+        System.out.println();
+        System.out.println("mean:   " + accumulator.mean());
+        System.out.println("var:    " + accumulator.var());
+        System.out.println("stddev: " + accumulator.stddev());
     }
 
     public static class Accumulator
---
+++ src/main/java/algs/Algs_1_2_19_StringParsing.java
@@ -0,0 +1,78 @@
+package algs;
+
+/*
+1.2.19 Parsing. Develop the parse constructors for your  Date and  Transaction im-
+plementations of Exercise 1.2.13 that take a single  String argument to specify the
+initialization values, using the formats given in the table below.
+
+Partial solution:
+
+public Date(String date)
+{
+    String[] fields = date.split("/");
+    month = Integer.parseInt(fields[0]);
+    day = Integer.parseInt(fields[1]);
+    year = Integer.parseInt(fields[2]);
+}
+
+Date - integers separated by slashes
+    5/22/1939
+Transaction - customer, date, and amount, separated by whitespace
+    Turing 5/22/1939 11.99
+    
+* */
+
+public class Algs_1_2_19_StringParsing
+{
+    public static void main(String[] args) throws Exception
+    {
+        Transaction transaction = new Transaction("Anon 6/4/2017 500.77");
+        System.out.println(transaction);
+    }
+
+    public static class Date
+    {
+        private final int month;
+        private final int day;
+        private final int year;
+
+        public Date(String date)
+        {
+            String[] fields = date.split("/");
+            this.month = Integer.parseInt(fields[0]);
+            this.day = Integer.parseInt(fields[1]);
+            this.year = Integer.parseInt(fields[2]);
+        }
+
+        public String toString()
+        {
+            return month + "/" + day + "/" + year;
+        }
+    }
+
+    private static class Transaction
+    {
+        private final String who;
+        private final Date when;
+        private final double amount;
+
+        public Transaction(String transaction)
+        {
+            String[] fields = transaction.split(" ");
+
+            this.who = fields[0];
+            this.when = new Date(fields[1]);
+            this.amount = Double.parseDouble(fields[2]);
+        }
+
+        @Override
+        public String toString()
+        {
+            return "Transaction{" +
+                    "who='" + who + '\'' +
+                    ", when=" + when +
+                    ", amount=" + amount +
+                    '}';
+        }
+    }
+}
