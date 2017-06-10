130e2e0324495ac0cb6f07e1c7c17dd9df767e02
Eric Hicks <ehicks05@yahoo.com>
2017-06-10 11:38:46 -0400

    Added wip for 1.3.31.

--- src/main/java/algs/ch1/s3/Algs_1_3_31_DoubleNode.java
+++ src/main/java/algs/ch1/s3/Algs_1_3_31_DoubleNode.java
@@ -13,6 +13,71 @@ public class Algs_1_3_31_DoubleNode
 {
     public static void main(String[] args) throws Exception
     {
+        DoubleNode first = new DoubleNode(2);
+        DoubleNode second = new DoubleNode(0);
+        DoubleNode third = new DoubleNode(9);
+        DoubleNode fourth = new DoubleNode(7);
 
+        List list = new List(first);
+        list.insertEnd(second);
+        list.insertEnd(third);
+        list.insertEnd(fourth);
+
+
+    }
+
+    public static class List
+    {
+        DoubleNode first;
+
+        static void insertBeginning(DoubleNode node)
+        {
+
+        }
+
+        static void insertEnd(DoubleNode node)
+        {
+
+        }
+
+        static DoubleNode removeBeginning()
+        {
+
+        }
+
+        static DoubleNode removeEnd()
+        {
+
+        }
+
+        static void insertBeforeGiven(DoubleNode newNode, DoubleNode given)
+        {
+
+        }
+
+        static void insertAfterGiven(DoubleNode newNode, DoubleNode given)
+        {
+
+        }
+
+        static DoubleNode removeGiven(DoubleNode given)
+        {
+
+        }
+
+        public List(DoubleNode first) {
+            this.first = first;
+        }
+    }
+
+    public static class DoubleNode
+    {
+        int id;
+        DoubleNode prev;
+        DoubleNode next;
+
+        public DoubleNode(int id) {
+            this.id = id;
+        }
     }
 }
